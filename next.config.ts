de0d59f9fbe2308641dd46686b389e017884dd66
tsebastian-oss <tsebastian@fen.uchile.cl>
2026-08-04 15:55:20 -0400

    Allow supermarket product images

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,14 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = { reactStrictMode: true };
+const nextConfig: NextConfig = {
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      { protocol: "https", hostname: "jumbo.vteximg.com.br" },
+      { protocol: "https", hostname: "santaisabel.vteximg.com.br" },
+      { protocol: "https", hostname: "i5.walmartimages.cl" },
+    ],
+  },
+};
 
 export default nextConfig;
