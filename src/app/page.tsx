c1c077a6bebbb0cd6f5851faabc539674634197b
tsebastian-oss <tsebastian@fen.uchile.cl>
2026-08-06 14:42:58 -0400

    Keep the executive home free of legacy overlays

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,8 @@
-import DataExportPortal from "./DataExportPortal";
 import ExecutiveDashboardV2 from "./ExecutiveDashboardV2";
 import IndustryGate from "./IndustryGate";
 
 export default function Home() {
   return <IndustryGate>
     <ExecutiveDashboardV2 />
-    <DataExportPortal />
   </IndustryGate>;
 }
