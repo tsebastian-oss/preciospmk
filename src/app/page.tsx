b65fa3cf4fc9cbfa742222ed4e6e08abd611033b
tsebastian-oss <tsebastian@fen.uchile.cl>
2026-08-06 14:47:21 -0400

    Use the exact approved dashboard composition

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
-import ExecutiveDashboardV2 from "./ExecutiveDashboardV2";
+import ExecutiveDashboardExact from "./ExecutiveDashboardExact";
 import IndustryGate from "./IndustryGate";
 
 export default function Home() {
   return <IndustryGate>
-    <ExecutiveDashboardV2 />
+    <ExecutiveDashboardExact />
   </IndustryGate>;
 }
