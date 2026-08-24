cc669fd0cd5391e7dc76f7f4fe2bd4121fc14d52
tsebastian-oss <tsebastian@fen.uchile.cl>
2026-08-24 18:11:03 -0400

    Load global dropdown contrast overrides

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import "./mobile-tablet-overrides.css";
 import "./desktop-layout-overrides-v2.css";
 import "./pharmacy-status-overrides.css";
+import "./select-contrast-overrides.css";
 
 export const viewport: Viewport = {
   width: "device-width",
