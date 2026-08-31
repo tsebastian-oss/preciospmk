04eb04d0228189f02874dd8320f8024d140d4233
tsebastian-oss <tsebastian@fen.uchile.cl>
2026-08-31 16:13:59 -0400

    Add authorized Victorinox history API

---
+++ src/app/api/brands/victorinox/history/route.ts
@@ -0,0 +1,5 @@
+import { handleVictorinoxCompetitionHistory } from "@/lib/brands-competition-history";
+
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+export const GET = handleVictorinoxCompetitionHistory;
