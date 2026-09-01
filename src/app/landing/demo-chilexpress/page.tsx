528b2700495bcbbdbce45879aeebb3d1556f2c48
tsebastian-oss <tsebastian@fen.uchile.cl>
2026-09-01 10:30:35 -0400

    Make Chilexpress demo directly viewable

--- src/app/landing/demo-chilexpress/page.tsx
+++ src/app/landing/demo-chilexpress/page.tsx
@@ -1,5 +1,1 @@
-import { redirect } from "next/navigation";
-
-export default function ChilexpressDemoEntryPage() {
-  redirect("/login?next=/panel/chilexpress");
-}
+export { default } from "../../panel/chilexpress/page";
