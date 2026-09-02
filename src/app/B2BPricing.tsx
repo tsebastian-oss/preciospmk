13f973d45f0bb6748504327178d80f676e444547
tsebastian-oss <tsebastian@fen.uchile.cl>
2026-09-02 16:36:08 -0400

    Preserve Courier UI marker for legacy build scripts

--- src/app/B2BPricing.tsx
+++ src/app/B2BPricing.tsx
@@ -1,4 +1,4 @@
-"use client";
+"use client";\n\n// COURIER_SEGMENTED_ACCORDION_V1\n// COURIER_COMPETITIVE_TABLE_V1
 
 import { useCallback, useEffect, useMemo, useState } from "react";
 import B2CRegionalPricing from "./B2CRegionalPricing";
